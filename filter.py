d29f145fe7a8aa4a22a5b8290be2e9bfd78af522
Breno Freitas <brenolimadefreitas@gmail.com>
2017-11-01 19:26:33 -0400

    Add output filtering script

---
+++ filter.py
@@ -0,0 +1,23 @@
+import sys
+
+document = ''
+
+while True:
+  line = sys.stdin.readline()
+
+  if line == '':
+    break
+
+  if line.find('confusion_matrix') >= 0:
+    continue
+
+  if line.find('precision') >= 0:
+    continue
+
+  if line.find('recall') >= 0:
+    continue
+
+  if line.find(':') >= 0:
+    document += line
+
+print document
