Fix overflow bug

online.py
@@ -80,7 +80,11 @@ def should_give_feedback(epoch, head_epoch, n_mistakes):
     return False
 
   if FEEDBACK['expon']:
-    return (epoch - head_epoch) >= np.floor(float(args.feedback[1:]) ** (n_mistakes))
+    try:
+      return (epoch - head_epoch) >= np.floor(float(args.feedback[1:]) ** (n_mistakes))
+    except:
+      FEEDBACK['expon'] = True
+      return False
 
   if FEEDBACK['proba']:
     return np.random.random() <= float(args.feedback)